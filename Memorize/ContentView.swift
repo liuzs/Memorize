efd8dbe174c39b627482a08175611b8c811724d2
lzs <ggg19960720@gmail.com>
2020-09-08 18:25:36 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by sanxi on 2020/9/8.
+//  Copyright © 2020 sanxi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
