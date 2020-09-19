a7e836319d1b2faa7ba4fea19a5f534da8bc31a4
lzs <ggg19960720@gmail.com>
2020-09-20 04:57:14 +0800

    使用协议重新实现了识别卡片id

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,10 +12,11 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(items: viewModel.cards) { card in
+        Grid(viewModel.cards) { card in
                 Cardviwe(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
                 }
+                .padding()
         }
             .foregroundColor(Color.orange)
             .padding()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #  Memorize
-一个基于 swiftUI 的翻卡片记忆力小游戏
+一个基于 SwiftUI 的翻卡片记忆力小游戏
